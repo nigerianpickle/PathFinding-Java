7ce99729b715a48ca97bbeb63b0c4042cc4f8e07
Daniel Nwogo <118936910+nigerianpickle@users.noreply.github.com>
2023-06-29 19:36:25 +0100

    Add files via upload

---
+++ KeyHandler.java
@@ -0,0 +1,29 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+    DemoPanel dp;
+
+    public KeyHandler(DemoPanel dp){
+        this.dp=dp;
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code=e.getKeyCode();
+        if (code==KeyEvent.VK_ENTER) {
+            // dp.search();
+            dp.autoSearch();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+    }
+    
+}
